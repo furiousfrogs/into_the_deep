09eec0b50e9c0c027f16d2b72fe8562ebcc5e9c3
Furious Frogs Robotics <furiousfrogsrobotics@gmail.com>
2025-01-17 21:25:31 -0800

    modified initialization.java with previous design configuration, have not yet implemented new intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeMethodized/Communicate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeMethodized/Communicate.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.codeMethodized;
 import org.firstinspires.ftc.teamcode.codeMethodized.VarAll;
 public class Communicate {
     public void Talk(Initialization initModule){
-        if(currentGod[0]=0.0){
+        if(initModule.currentGod[0] == 0.0){
 
         }
     }
