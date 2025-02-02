7727aef7c383bb129aa855d04e2d1c0f50d6eee0
Furious_Frogs <furiousfrogs@gmail.com>
2025-02-01 22:16:30 -0800

    just some small changes to codeMethodized

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeMethodized/Take.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeMethodized/Take.java
@@ -13,39 +13,26 @@ public class Take {
         }
         //manual controlled
         if(initModule.currentGamepad2.left_bumper && !initModule.previousGamepad2.left_bumper){
-            if(initModule.currentGod[13]>-0.2){
-            initModule.currentGod[13]=-0.8;
+            if(initModule.currentGod[12]>-0.2){
+            initModule.currentGod[12]=-0.8;
             }else{
-                initModule.currentGod[13]=0;
+                initModule.currentGod[12]=0;
             }
         }
         if(initModule.currentGamepad2.right_bumper && !initModule.previousGamepad2.right_bumper){
-            if(initModule.currentGod[13]<0.2){
-                initModule.currentGod[13]=0.8;
+            if(initModule.currentGod[12]<0.2){
+                initModule.currentGod[12]=0.8;
                 }else{
-                    initModule.currentGod[13]=0;
+                    initModule.currentGod[12]=0;
                 }
         }
         initModule.currentGod[9]=-initModule.currentGamepad2.left_stick_x;
         initModule.currentGod[10]=-initModule.currentGamepad2.left_stick_y;
         initModule.currentGod[11]=-initModule.currentGamepad2.left_stick_y;
-        if(initModule.currentGamepad2.circle && ! initModule.previousGamepad2.circle){
-            if(initModule.currentGod[12]==VarAll.inIdle){
-                initModule.currentGod[12]=VarAll.inDown;
-            }else{
-                initModule.currentGod[12]=VarAll.inIdle;
-            }
-        }
-        if(initModule.currentGamepad2.cross && !initModule.previousGamepad2.cross){
+        if(initModule.currentGamepad2.triangle && !initModule.previousGamepad2.triangle){
             initModule.currentGod[8]=VarAll.clawOpen;
         }
-        
-        //state controlled
-        if(state==0.0F){
-
-        }
-        //safety controlled
-        //reset controlled
+        if(initModule.currentGamepad2.square && !initModule.previousGamepad2.square)
     }
     
 }
