f71ac1a93f83efcf99a1ec4fba1ad1c3af4a4eef
Hao Cui <decubingexpert@gmail.com>
2025-02-05 16:45:52 -0800

    Speed scaling, input scaling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeMethodized/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codeMethodized/Drive.java
@@ -2,18 +2,41 @@ package org.firstinspires.ftc.teamcode.codeMethodized;
 
 public class Drive {
     public void Drive(Initialization initModule){
+
+        //Taking input
         double y=-initModule.currentGamepad1.left_stick_y;
-        double x=initModule.currentGamepad1.left_stick_x*1.3; //TUNE THE COUNTER STRAFING
+        double x= initModule.currentGamepad1.left_stick_x*1.3; 
+
+        //Adjusting scale
+        if (x > -0.2 && x < 0.2) {
+            x = 0;
+        }
+        if (y > -0.2 && y < 0.2) {
+            y = 0;
+        }
+        
+        
         double rx= (initModule.currentGamepad1.right_trigger-initModule.currentGamepad1.left_trigger);
+
+
         double denominator=Math.max(Math.abs(y)+Math.abs(x)+Math.abs(rx),1);
-        double frontLeftPower = (y+x+rx)/denominator;
-        double backLeftPower = (y-x+rx)/denominator;
-        double frontRightPower = (y-x-rx)/denominator;
-        double backRightPower = (y+x-rx)/denominator;
+        
+        //set constant to max power if its above a certain x limit
+        double constant = 0;
+        if (Math.abs(x) >  0.8){
+            constant = 1.0;
+        }
+
+        double frontLeftPower = Math.max((y+x+rx)/denominator,constant);
+        double backLeftPower = Math.max((y-x+rx)/denominator,constant);
+        double frontRightPower = Math.max((y-x-rx)/denominator,constant);
+        double backRightPower = Math.max((y+x-rx)/denominator,constant);
+
+
+
         initModule.currentGod[0]=frontLeftPower;
         initModule.currentGod[1]=backLeftPower;
         initModule.currentGod[2]=frontRightPower;
         initModule.currentGod[3]=backRightPower;
     }
 }
-
